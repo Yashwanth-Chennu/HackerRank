ec918d9f4e36d919c275a3c1e038c9f5ab0df005
Yashwanth Chennu <yashwanthchennu1412@gmail.com>
2020-09-05 23:32:17 +0530

    Add files via upload

---
+++ Java/Java Inheritance I.java
@@ -0,0 +1,40 @@
+https://github.com/Yashwanth-Chennu
+
+Problem Statement:https://www.hackerrank.com/challenges/java-inheritance-1/problem
+
+Solution: 
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Animal{
+	void walk()
+	{
+		System.out.println("I am walking");
+	}
+}
+class Bird extends Animal
+{
+    void sing(){
+        System.out.println("I am singing");
+    }
+	void fly()
+	{
+		System.out.println("I am flying");
+	}
+}
+
+public class Solution{
+
+   public static void main(String args[]){
+
+	  Bird bird = new Bird();
+	  bird.walk();
+	  bird.fly();
+      bird.sing();
+	
+   }
+}
