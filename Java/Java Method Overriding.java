f80ecefced9d1922bc7cbd47e75a8adc1b680004
Yashwanth Chennu <yashwanthchennu1412@gmail.com>
2020-09-08 18:42:49 +0530

    Add files via upload

---
+++ Java/Java Method Overriding.java
@@ -0,0 +1,43 @@
+https://github.com/Yashwanth-Chennu
+
+Problem Statement: https://www.hackerrank.com/challenges/java-method-overriding/problem?h_r=next-challenge&h_v=zen
+
+Solution: 
+
+import java.util.*;
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    // Write your overridden getNumberOfTeamMembers method here
+    @Override
+    void getNumberOfTeamMembers(){
+        System.out.println("Each team has 11 players in " + getName() );
+    }
+
+}
+
+public class Solution{
+	
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+	}
+}
